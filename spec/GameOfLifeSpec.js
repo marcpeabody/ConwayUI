9f50e3c784152af494b89a08631c18aba8c6ff2c
Marc Peabody <marcpeabody@yahoo.com>
2016-03-13 13:19:39 -0400

    Parsing and rendering a game board
    Taking an input that is more textual in nature and converting it into a two dimensional array internally.

--- spec/GameOfLifeSpec.js
+++ spec/GameOfLifeSpec.js
@@ -1,14 +1,38 @@
 describe("GameOfLife", function(){
+  var textualBoard = [
+        "......O.",
+        "OOO...O.",
+        "......O.",
+        "........",
+        "...OO...",
+        "...OO..."
+      ],
+      parsedBoard = [
+        ['.','.','.','.','.','.','O','.'],
+        ['O','O','O','.','.','.','O','.'],
+        ['.','.','.','.','.','.','O','.'],
+        ['.','.','.','.','.','.','.','.'],
+        ['.','.','.','O','O','.','.','.'],
+        ['.','.','.','O','O','.','.','.'],
+      ]
+  ;
+
   it("can return the same board display it was given", function(){
-    var input = [
-      "......O.",
-      "OOO...O.",
-      "......O.",
-      "........",
-      "...OO...",
-      "...OO..."
-    ],
-    game = new GameOfLife(input);
-    expect(game.drawCurrentBoard()).toBe(input)
+    var game = new GameOfLife(textualBoard);
+    expect(game.renderCurrentBoard()).toEqual(textualBoard);
+  });
+
+  it("can parse text board into two dimensional array", function(){
+    var game = new GameOfLife(),
+        arrays = game.parseBoard(textualBoard);
+
+    expect(arrays).toEqual(parsedBoard);
+  });
+
+  it("can render board into text array", function(){
+    var game = new GameOfLife(),
+        renderedBoard = game.renderBoard(parsedBoard);
+
+    expect(renderedBoard).toEqual(textualBoard);
   });
 });
--- src/GameOfLife.js
+++ src/GameOfLife.js
@@ -1,9 +1,36 @@
 var GameOfLife = (function() {
   var Game = function(input){
-    this.currentBoardState = input;
+    this.currentBoardState = this.parseBoard(input);
   };
-  Game.prototype.drawCurrentBoard = function(){
-    return this.currentBoardState;
+
+  Game.prototype.parseBoard = function(input) {
+    var arrays = [],
+        row;
+
+    for (var i in input) {
+      arrays[i] = []; // 2nd dimension
+
+      row = input[i];
+      for (var j in row) {
+        arrays[i][j] = row.charAt(j);
+      }
+    }
+    return arrays;
+  }
+
+  Game.prototype.renderCurrentBoard = function() {
+    return this.renderBoard(this.currentBoardState);
+  }
+
+  Game.prototype.renderBoard = function(arrays) {
+    // return this.currentBoardState;
+    var textArray = [];
+
+    for (var i in arrays) {
+      textArray[i] = arrays[i].join('');
+    }
+
+    return textArray;
   }
 
   return Game;
